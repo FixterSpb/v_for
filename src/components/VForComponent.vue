ba1b22dbe3a68f763bebfc18dbe4883094614e5f
Mladshev Evgeniy <fixter.spb@gmail.com>
2021-08-17 22:01:12 +0300

    v-for

---
+++ src/components/VForComponent.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+      style="
+          border: 2px solid gray;
+          padding: 20px;
+          text-align: left;
+          margin: 10px 50px;
+          border-radius: 10px;
+        "
+      >
+    {{ title }}
+    <button @click="$emit('delete-item', id )">Удалить</button>
+    <div
+        style="margin-top: 15px;
+        margin-left: 50px;
+        border: 4px dotted red;
+        padding: 5px;
+    ">
+      <div
+          v-for="(item, idx) in data"
+          :key="idx"
+      >{{ item }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VForComponent",
+  props: {
+    id: {
+      type: Number
+    },
+    idx: {
+      type: Number
+    },
+    title: {
+      type: String
+    },
+    status: {
+      type: Number
+    }
+  },
+  data() {
+    return {
+      data: []
+    }
+  },
+  mounted (){
+    console.log(this.status)
+    switch (this.status) {
+      case 1:
+        this.data.push('Вложение 1 для записи № ' + this.id)
+        break
+      case 2:
+        this.data.push('Вложение 1 для записи № ' + this.id)
+        this.data.push('Вложение 2 для записи № ' + this.id)
+        break
+      case 3:
+        this.data.push('Вложение 1 для записи № ' + this.id)
+        this.data.push('Вложение 2 для записи № ' + this.id)
+        this.data.push('Вложение 3 для записи № ' + this.id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/helpers/storage.js
@@ -0,0 +1,37 @@
+export const add = data => {
+    const dataStore = get()
+    data.id = getId(dataStore)
+    data.title += data.id
+    dataStore.unshift(data)
+    localStorage.setItem('data', JSON.stringify(dataStore))
+    return [...dataStore]
+}
+
+export const get = () => {
+    const dataJson = localStorage.getItem('data')
+    if (!dataJson) {
+        return []
+    }
+    console.log(JSON.parse(dataJson));
+    return JSON.parse(dataJson)
+}
+
+export const del = (id) => {
+    const dataJson = localStorage.getItem('data')
+    console.log(dataJson, id)
+    if (!dataJson) return
+
+    const data = JSON.parse(dataJson)
+    data.splice(data.indexOf(data.find(el => el.id === id)), 1)
+    localStorage.setItem('data', JSON.stringify(data))
+    return [...data]
+}
+
+const getId = (data) => {
+    if (data.length === 0) return 1
+    const tempData = [...data]
+    const id = tempData.sort((a, b) => b.id - a.id)[0].id + 1
+    console.log(id);
+    return id
+    // return tempData.sort((a, b) => a.id - b.id).shift
+}
